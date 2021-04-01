14ab0c7aa94086af787618248ec5ca5ffce0d320
seanmul10 <36199832+seanmul10@users.noreply.github.com>
2021-04-02 00:10:13 +0100

    Basic search by date layout
    Written a basic portrait layout for the search by date activity and allowed these to be displayed when searching by date
    
    There is still a lot of work to do to ensure the layout appears as desired

--- EQuakeStarterCode/app/src/main/java/org/me/gcu/equakestartercode/SearchByDateActivity.java
+++ EQuakeStarterCode/app/src/main/java/org/me/gcu/equakestartercode/SearchByDateActivity.java
@@ -20,6 +20,7 @@ public class SearchByDateActivity extends EarthquakeActivity implements DatePick
 {
     List<Earthquake> earthquakeRange;
 
+    // Date search elements
     DatePickerDialog datePickerDialog1;
     DatePickerDialog datePickerDialog2;
 
@@ -27,6 +28,37 @@ public class SearchByDateActivity extends EarthquakeActivity implements DatePick
     TextView dateEditText2;
 
     Button searchButton;
+
+    // Earthquake text views
+    TextView largestEqLocation;
+    TextView largestEqDate;
+    TextView largestEqMagnitude;
+
+    TextView northEqLocation;
+    TextView northEqDate;
+    TextView northEqLatLon;
+
+    TextView eastEqLocation;
+    TextView eastEqDate;
+    TextView eastEqLatLon;
+
+    TextView southEqLocation;
+    TextView southEqDate;
+    TextView southEqLatLon;
+
+    TextView westEqLocation;
+    TextView westEqDate;
+    TextView westEqLatLon;
+
+    TextView shallowestEqLocation;
+    TextView shallowestEqDate;
+    TextView shallowestEqDepth;
+
+    TextView deepestEqLocation;
+    TextView deepestEqDate;
+    TextView deepestEqDepth;
+
+    TextView lastBuildDate;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -36,6 +68,7 @@ public class SearchByDateActivity extends EarthquakeActivity implements DatePick
 
         earthquakeRange = new ArrayList<>();
 
+        // Date search elements
         Calendar calendar = Calendar.getInstance();
         datePickerDialog1 = new DatePickerDialog(this, SearchByDateActivity.this, calendar.get(Calendar.YEAR), calendar.get(Calendar.MONTH), calendar.get(Calendar.DAY_OF_MONTH));
         datePickerDialog2 = new DatePickerDialog(this, SearchByDateActivity.this, calendar.get(Calendar.YEAR), calendar.get(Calendar.MONTH), calendar.get(Calendar.DAY_OF_MONTH));
@@ -50,6 +83,39 @@ public class SearchByDateActivity extends EarthquakeActivity implements DatePick
 
         SetEditText(dateEditText1, calendar);
         SetEditText(dateEditText2, calendar);
+
+        // Earthquake display elements
+        largestEqLocation = (TextView)findViewById(R.id.largestEqLocation);
+        largestEqDate = (TextView)findViewById(R.id.largestEqDate);
+        largestEqMagnitude = (TextView)findViewById(R.id.largestEqMagnitude);
+
+        northEqLocation = (TextView)findViewById(R.id.northEqLocation);;
+        northEqDate = (TextView)findViewById(R.id.northEqDate);
+        northEqLatLon = (TextView)findViewById(R.id.northEqLatLon);
+
+        eastEqLocation = (TextView)findViewById(R.id.eastEqLocation);
+        eastEqDate = (TextView)findViewById(R.id.eastEqLDate);
+        eastEqLatLon = (TextView)findViewById(R.id.eastEqLatLon);
+
+        southEqLocation = (TextView)findViewById(R.id.southEqLocation);
+        southEqDate = (TextView)findViewById(R.id.southEqDate);
+        southEqLatLon = (TextView)findViewById(R.id.southEqLatLon);
+
+        westEqLocation = (TextView)findViewById(R.id.westEqLocation);
+        westEqDate = (TextView)findViewById(R.id.westEqDate);
+        westEqLatLon = (TextView)findViewById(R.id.westEqLatLon);
+
+        shallowestEqLocation = (TextView)findViewById(R.id.shallowestEqLocation);
+        shallowestEqDate = (TextView)findViewById(R.id.shallowestEqDate);
+        shallowestEqDepth = (TextView)findViewById(R.id.shallowestEqDepth);
+
+        deepestEqLocation = (TextView)findViewById(R.id.deepestEqLocation);
+        deepestEqDate = (TextView)findViewById(R.id.deepestEqDate);
+        deepestEqDepth = (TextView)findViewById(R.id.deepestEqDepth);
+
+        lastBuildDate = (TextView)findViewById(R.id.buildDate);
+
+        lastBuildDate.setText("Earthquake data correct as of " + EarthquakeData.getLastBuildDate());
     }
 
     @Override
@@ -77,7 +143,49 @@ public class SearchByDateActivity extends EarthquakeActivity implements DatePick
         else if (view == searchButton) {
             earthquakeRange = EarthquakeData.getEarthquakesInRange(getComparableDate(datePickerDialog1.getDatePicker()), getComparableDate(datePickerDialog2.getDatePicker()));
             Log.d("DateSearch", "Found " + earthquakeRange.size() + " earthquakes in the given range.");
+            UpdateEarthquakeData();
         }
+    }
+
+    private void UpdateEarthquakeData() {
+        if (earthquakeRange.size() == 0) {
+            // No earthquakes found in this range
+            return;
+        }
+        Earthquake largestEarthquake = EarthquakeData.getBiggestEarthquake(earthquakeRange);
+        largestEqLocation.setText(largestEarthquake.getLocation());
+        largestEqDate.setText(largestEarthquake.getDate());
+        largestEqMagnitude.setText(Float.toString(largestEarthquake.getMagnitude()));
+
+        Earthquake northernmostEarthquake = EarthquakeData.getMostNorthernEarthquake(earthquakeRange);
+        northEqLocation.setText(northernmostEarthquake.getLocation());
+        northEqDate.setText(northernmostEarthquake.getDate());
+        northEqLatLon.setText(northernmostEarthquake.getLatitude() + ", " + northernmostEarthquake.getLongitude());
+
+        Earthquake easternmostEarthquake = EarthquakeData.getMostEasterlyEarthquake(earthquakeRange);
+        eastEqLocation.setText(easternmostEarthquake.getLocation());
+        eastEqDate.setText(easternmostEarthquake.getDate());
+        eastEqLatLon.setText(easternmostEarthquake.getLatitude() + ", " + easternmostEarthquake.getLongitude());
+
+        Earthquake southernmostEarthquake = EarthquakeData.getMostSoutherlyEarthquake(earthquakeRange);
+        southEqLocation.setText(southernmostEarthquake.getLocation());
+        southEqDate.setText(southernmostEarthquake.getDate());
+        southEqLatLon.setText(southernmostEarthquake.getLatitude() + ", " + southernmostEarthquake.getLongitude());
+
+        Earthquake westernmostEarthquake = EarthquakeData.getMostWesterlyEarthquake(earthquakeRange);
+        westEqLocation.setText(westernmostEarthquake.getLocation());
+        westEqDate.setText(westernmostEarthquake.getDate());
+        westEqLatLon.setText(westernmostEarthquake.getLatitude() + ", " + westernmostEarthquake.getLongitude());
+
+        Earthquake shallowestEarthquake = EarthquakeData.getShallowestEarthquake(earthquakeRange);
+        shallowestEqLocation.setText(shallowestEarthquake.getLocation());
+        shallowestEqDate.setText(shallowestEarthquake.getDate());
+        shallowestEqDepth.setText(shallowestEarthquake.getDepth() + "km");
+
+        Earthquake deepestEarthquake = EarthquakeData.getDeepestEarthquake(earthquakeRange);
+        deepestEqLocation.setText(deepestEarthquake.getLocation());
+        deepestEqDate.setText(deepestEarthquake.getDate());
+        deepestEqDepth.setText(deepestEarthquake.getDepth() + "km");
     }
 
     private void SetEditText(TextView editText, Calendar calendar) {
