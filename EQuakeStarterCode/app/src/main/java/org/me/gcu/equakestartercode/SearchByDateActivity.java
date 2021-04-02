0d5794b68874656825510225d238205cf08aaa97
seanmul10 <36199832+seanmul10@users.noreply.github.com>
2021-04-02 10:33:12 +0100

    Added landscape search by date layout
    Layouts for both rotation modes are unfinished

--- EQuakeStarterCode/app/src/main/java/org/me/gcu/equakestartercode/SearchByDateActivity.java
+++ EQuakeStarterCode/app/src/main/java/org/me/gcu/equakestartercode/SearchByDateActivity.java
@@ -87,31 +87,31 @@ public class SearchByDateActivity extends EarthquakeActivity implements DatePick
         // Earthquake display elements
         largestEqLocation = (TextView)findViewById(R.id.largestEqLocation);
         largestEqDate = (TextView)findViewById(R.id.largestEqDate);
-        largestEqMagnitude = (TextView)findViewById(R.id.largestEqMagnitude);
+        //largestEqMagnitude = (TextView)findViewById(R.id.largestEqMagnitude);
 
         northEqLocation = (TextView)findViewById(R.id.northEqLocation);;
         northEqDate = (TextView)findViewById(R.id.northEqDate);
-        northEqLatLon = (TextView)findViewById(R.id.northEqLatLon);
+        //northEqLatLon = (TextView)findViewById(R.id.northEqLatLon);
 
         eastEqLocation = (TextView)findViewById(R.id.eastEqLocation);
-        eastEqDate = (TextView)findViewById(R.id.eastEqLDate);
-        eastEqLatLon = (TextView)findViewById(R.id.eastEqLatLon);
+        eastEqDate = (TextView)findViewById(R.id.eastEqDate);
+        //eastEqLatLon = (TextView)findViewById(R.id.eastEqLatLon);
 
         southEqLocation = (TextView)findViewById(R.id.southEqLocation);
         southEqDate = (TextView)findViewById(R.id.southEqDate);
-        southEqLatLon = (TextView)findViewById(R.id.southEqLatLon);
+        //southEqLatLon = (TextView)findViewById(R.id.southEqLatLon);
 
         westEqLocation = (TextView)findViewById(R.id.westEqLocation);
         westEqDate = (TextView)findViewById(R.id.westEqDate);
-        westEqLatLon = (TextView)findViewById(R.id.westEqLatLon);
+        //westEqLatLon = (TextView)findViewById(R.id.westEqLatLon);
 
         shallowestEqLocation = (TextView)findViewById(R.id.shallowestEqLocation);
         shallowestEqDate = (TextView)findViewById(R.id.shallowestEqDate);
-        shallowestEqDepth = (TextView)findViewById(R.id.shallowestEqDepth);
+        //shallowestEqDepth = (TextView)findViewById(R.id.shallowestEqDepth);
 
         deepestEqLocation = (TextView)findViewById(R.id.deepestEqLocation);
         deepestEqDate = (TextView)findViewById(R.id.deepestEqDate);
-        deepestEqDepth = (TextView)findViewById(R.id.deepestEqDepth);
+        //deepestEqDepth = (TextView)findViewById(R.id.deepestEqDepth);
 
         lastBuildDate = (TextView)findViewById(R.id.buildDate);
 
@@ -155,37 +155,37 @@ public class SearchByDateActivity extends EarthquakeActivity implements DatePick
         Earthquake largestEarthquake = EarthquakeData.getBiggestEarthquake(earthquakeRange);
         largestEqLocation.setText(largestEarthquake.getLocation());
         largestEqDate.setText(largestEarthquake.getDate());
-        largestEqMagnitude.setText(Float.toString(largestEarthquake.getMagnitude()));
+        //largestEqMagnitude.setText(Float.toString(largestEarthquake.getMagnitude()));
 
         Earthquake northernmostEarthquake = EarthquakeData.getMostNorthernEarthquake(earthquakeRange);
         northEqLocation.setText(northernmostEarthquake.getLocation());
         northEqDate.setText(northernmostEarthquake.getDate());
-        northEqLatLon.setText(northernmostEarthquake.getLatitude() + ", " + northernmostEarthquake.getLongitude());
+        //northEqLatLon.setText(northernmostEarthquake.getLatitude() + ", " + northernmostEarthquake.getLongitude());
 
         Earthquake easternmostEarthquake = EarthquakeData.getMostEasterlyEarthquake(earthquakeRange);
         eastEqLocation.setText(easternmostEarthquake.getLocation());
         eastEqDate.setText(easternmostEarthquake.getDate());
-        eastEqLatLon.setText(easternmostEarthquake.getLatitude() + ", " + easternmostEarthquake.getLongitude());
+        //eastEqLatLon.setText(easternmostEarthquake.getLatitude() + ", " + easternmostEarthquake.getLongitude());
 
         Earthquake southernmostEarthquake = EarthquakeData.getMostSoutherlyEarthquake(earthquakeRange);
         southEqLocation.setText(southernmostEarthquake.getLocation());
         southEqDate.setText(southernmostEarthquake.getDate());
-        southEqLatLon.setText(southernmostEarthquake.getLatitude() + ", " + southernmostEarthquake.getLongitude());
+        //southEqLatLon.setText(southernmostEarthquake.getLatitude() + ", " + southernmostEarthquake.getLongitude());
 
         Earthquake westernmostEarthquake = EarthquakeData.getMostWesterlyEarthquake(earthquakeRange);
         westEqLocation.setText(westernmostEarthquake.getLocation());
         westEqDate.setText(westernmostEarthquake.getDate());
-        westEqLatLon.setText(westernmostEarthquake.getLatitude() + ", " + westernmostEarthquake.getLongitude());
+        //westEqLatLon.setText(westernmostEarthquake.getLatitude() + ", " + westernmostEarthquake.getLongitude());
 
         Earthquake shallowestEarthquake = EarthquakeData.getShallowestEarthquake(earthquakeRange);
         shallowestEqLocation.setText(shallowestEarthquake.getLocation());
         shallowestEqDate.setText(shallowestEarthquake.getDate());
-        shallowestEqDepth.setText(shallowestEarthquake.getDepth() + "km");
+        //shallowestEqDepth.setText(shallowestEarthquake.getDepth() + "km");
 
         Earthquake deepestEarthquake = EarthquakeData.getDeepestEarthquake(earthquakeRange);
         deepestEqLocation.setText(deepestEarthquake.getLocation());
         deepestEqDate.setText(deepestEarthquake.getDate());
-        deepestEqDepth.setText(deepestEarthquake.getDepth() + "km");
+        //deepestEqDepth.setText(deepestEarthquake.getDepth() + "km");
     }
 
     private void SetEditText(TextView editText, Calendar calendar) {
